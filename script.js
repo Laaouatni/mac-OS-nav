361c56613151a6b9eb63167c0812b86a312a503c
laaouatni <laaouatni.anas@outlook.it>
2022-01-16 11:43:22 +0100

    TEST se onclick non permette touchstart di iniziar

--- script.js
+++ script.js
@@ -9,62 +9,63 @@ let icons = document.querySelectorAll('.icon');
 
 
 icons.forEach((item, index) => {
-    // hover icon
-    icons[index].addEventListener('mouseover', function(ePC) {
-
-        // medium
-        let next1 = index + 1;
-        let prima1 = index - 1;
-
-        // small
-        let next2 = index + 2;
-        let prima2 = index - 2;
-
-        if (ePC.target == icons[0]) {
-            // normal
-            this.style.transform = bigIcon;
+    /*     // hover icon
+        icons[index].addEventListener('mouseover', function(ePC) {
 
             // medium
-            icons[next1].style.transform = middleIcon;
+            let next1 = index + 1;
+            let prima1 = index - 1;
 
             // small
-            icons[next2].style.transform = smallIcon;
-        } else if (ePC.target == icons[icons.length - 1]) {
-            // normal
-            this.style.transform = bigIcon;
+            let next2 = index + 2;
+            let prima2 = index - 2;
 
-            // medium
-            icons[prima1].style.transform = middleIcon;
+            if (ePC.target == icons[0]) {
+                // normal
+                this.style.transform = bigIcon;
 
-            // small
-            icons[prima2].style.transform = smallIcon;
-        } else {
-            // normal
-            this.style.transform = bigIcon;
+                // medium
+                icons[next1].style.transform = middleIcon;
 
-            // medium
-            icons[prima1].style.transform = middleIcon;
-            icons[next1].style.transform = middleIcon;
+                // small
+                icons[next2].style.transform = smallIcon;
+            } else if (ePC.target == icons[icons.length - 1]) {
+                // normal
+                this.style.transform = bigIcon;
 
-            // small
-            icons[prima2].style.transform = smallIcon;
-            icons[next2].style.transform = smallIcon;
-        }
-    });
+                // medium
+                icons[prima1].style.transform = middleIcon;
 
+                // small
+                icons[prima2].style.transform = smallIcon;
+            } else {
+                // normal
+                this.style.transform = bigIcon;
 
-    // not hover icon
-    icons[index].addEventListener('mouseout', function() {
-        icons.forEach((item1, hideIndex) => {
-            icons[hideIndex].style.transform = normalIcon;
+                // medium
+                icons[prima1].style.transform = middleIcon;
+                icons[next1].style.transform = middleIcon;
+
+                // small
+                icons[prima2].style.transform = smallIcon;
+                icons[next2].style.transform = smallIcon;
+            }
         });
-    });
 
+
+        // not hover icon
+        icons[index].addEventListener('mouseout', function() {
+            icons.forEach((item1, hideIndex) => {
+                icons[hideIndex].style.transform = normalIcon;
+            });
+        });
+     */
     /*****************************************************************/
 
     // hover icon
     icons[index].addEventListener('touchmove', function(eMobile) {
 
+        eMobile.preventDefault();
         // medium
         let next1 = index + 1;
         let prima1 = index - 1;
