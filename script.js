4c2c9ed870434671cb979fa3b30a7e1c8c3e9cdf
laaouatni <laaouatni.anas@outlook.it>
2022-01-16 11:27:04 +0100

    sopporto per mobile devices

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let icons = document.querySelectorAll('.icon');
 
 icons.forEach((item, index) => {
     // hover icon
-    icons[index].addEventListener('mouseover', function(e) {
+    icons[index].addEventListener('mouseover', function(ePC) {
 
         // medium
         let next1 = index + 1;
@@ -20,24 +20,97 @@ icons.forEach((item, index) => {
         let next2 = index + 2;
         let prima2 = index - 2;
 
+        if (ePC.target == icons[0]) {
+            // normal
+            this.style.transform = bigIcon;
 
-        // normal
-        this.style.transform = bigIcon;
+            // medium
+            icons[next1].style.transform = middleIcon;
 
-        // medium
-        icons[prima1].style.transform = middleIcon;
-        icons[next1].style.transform = middleIcon;
+            // small
+            icons[next2].style.transform = smallIcon;
+        } else if (ePC.target == icons[icons.length - 1]) {
+            // normal
+            this.style.transform = bigIcon;
 
-        // small
-        icons[prima2].style.transform = smallIcon;
-        icons[next2].style.transform = smallIcon;
+            // medium
+            icons[prima1].style.transform = middleIcon;
+
+            // small
+            icons[prima2].style.transform = smallIcon;
+        } else {
+            // normal
+            this.style.transform = bigIcon;
+
+            // medium
+            icons[prima1].style.transform = middleIcon;
+            icons[next1].style.transform = middleIcon;
+
+            // small
+            icons[prima2].style.transform = smallIcon;
+            icons[next2].style.transform = smallIcon;
+        }
     });
 
 
     // not hover icon
-    icons[index].addEventListener('mouseout', function(e) {
+    icons[index].addEventListener('mouseout', function() {
         icons.forEach((item1, hideIndex) => {
             icons[hideIndex].style.transform = normalIcon;
         });
     });
+
+    /*****************************************************************/
+
+    // hover icon
+    icons[index].addEventListener('touchstart', function(eMobile) {
+
+        // medium
+        let next1 = index + 1;
+        let prima1 = index - 1;
+
+        // small
+        let next2 = index + 2;
+        let prima2 = index - 2;
+
+        if (eMobile.target == icons[0]) {
+            // normal
+            this.style.transform = bigIcon;
+
+            // medium
+            icons[next1].style.transform = middleIcon;
+
+            // small
+            icons[next2].style.transform = smallIcon;
+        } else if (eMobile.target == icons[icons.length - 1]) {
+            // normal
+            this.style.transform = bigIcon;
+
+            // medium
+            icons[prima1].style.transform = middleIcon;
+
+            // small
+            icons[prima2].style.transform = smallIcon;
+        } else {
+            // normal
+            this.style.transform = bigIcon;
+
+            // medium
+            icons[prima1].style.transform = middleIcon;
+            icons[next1].style.transform = middleIcon;
+
+            // small
+            icons[prima2].style.transform = smallIcon;
+            icons[next2].style.transform = smallIcon;
+        }
+    });
+
+
+    // not hover icon
+    icons[index].addEventListener('touchend', function() {
+        icons.forEach((item1, hideIndex) => {
+            icons[hideIndex].style.transform = normalIcon;
+        });
+    });
+
 });
